8dc31fc398d1552218a9257e8452ae70e5932a0c
danstepanov <danhstepanov@gmail.com>
2022-06-02 16:43:40 -0400

    test mst-async-storage v0.3.0

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Pressable, Text, SafeAreaView, View } from 'react-native';
+import { types } from 'mobx-state-tree';
+import { withAsyncStorage } from 'mst-async-storage';
+import { observer } from "mobx-react-lite"
 
-export default function App() {
+export const NiceThingsModel = types
+  .model('NiceThings')
+  .props({
+    unicorns: true,
+    dragons: true,
+    cake: true,
+    spiders: false,
+    nickleback: false,
+  })
+  .actions((self) => ({
+    setSpiders(value) {
+      self.spiders = value;
+    },
+  }))
+  .extend(withAsyncStorage({ key: 'nice.things' }));
+
+// create your model as usual
+const happy = NiceThingsModel.create();
+
+const App = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        // now load the data from async storage
+        await happy.load();
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    })();
+  }, []);
+
+  function toggleSpiders() {
+    // and when you change something
+    happy.setSpiders(!happy.spiders);
+    // it will automatically save back to async storage!
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', padding: 16 }}>
+      <View>
+        <Text>{`Unicorns: ${happy.unicorns}`}</Text>
+        <Text>{`Dragons: ${happy.dragons}`}</Text>
+        <Text>{`Cake: ${happy.cake}`}</Text>
+        <Text style={{ fontWeight: 'bold' }}>{`Spiders: ${happy.spiders}`}</Text>
+        <Text>{`Nickleback: ${happy.nickleback}`}</Text>
+      </View>
+      <Pressable style={({ pressed }) => [{ backgroundColor: pressed ? 'black': 'blue'}, { padding: 10 }]} onPress={() => toggleSpiders()}>
+        <Text style={{color: 'white', fontWeight: 'bold' }}>Toggle Spiders</Text>  
+      </Pressable>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default observer(App);
